397f9f5647f82e16b95f717111ea799c8088f974
cakeller43 <cakeller43@gmail.com>
2015-10-21 13:24:51 -0500

    Network Integration
    10/21

--- Source/TCG/TCG.Build.cs
+++ Source/TCG/TCG.Build.cs
@@ -14,13 +14,14 @@ public class TCG : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
+		 PrivateDependencyModuleNames.Add("OnlineSubsystem");
+         PrivateDependencyModuleNames.Add("OnlineSubsystemUtils");
+         if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+         {
+             if (UEBuildConfiguration.bCompileSteamOSS == true)
+             {
+                 DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+             }
+         }
 	}
 }
